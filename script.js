9004ef0b7ef317e64b14375d08cfde010626ad14
Maksymilian Cieślak <maks.cieslak13@gmail.com>
2023-10-20 18:23:32 +0200

    today and goto buttons

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const calendar = document.querySelector(".calendar"),
     date = document.querySelector(".date"),
     daysContainer = document.querySelector(".days"),
     prev = document.querySelector(".prev"),
-    next = document.querySelector(".next");
+    next = document.querySelector(".next"),
+    todayBtn = document.querySelector(".today-btn"),
+    gotoBtn = document.querySelector(".goto-btn"),
+    dateInput = document.querySelector(".date-input");
 
 let today = new Date();
 let activeDay;
@@ -32,7 +35,7 @@ function initCalendar() {
     const lastDate = lastDay.getDate();
     const day = firstDay.getDay();
     const nextDays = 7 - lastDay.getDay() - 1;
-    
+
     date.innerHTML = months[month] + " " + year;
 
     let days = "";
@@ -40,13 +43,12 @@ function initCalendar() {
     for (let x = day; x > 0; x--) {
         days += `<div class="day prev-date">${prevDays - x + 1}</div>`;
     }
-    
+
     for (let i = 1; i <= lastDate; i++) {
-        if (i === new Date().getDate() && 
-            year === new Date().getFullYear() && 
-            month === new Date().getMonth()) 
-            {
-                days += `<div class="day today">${i}</div>`;
+        if (i === new Date().getDate() &&
+            year === new Date().getFullYear() &&
+            month === new Date().getMonth()) {
+            days += `<div class="day today">${i}</div>`;
         } else {
             days += `<div class="day">${i}</div>`;
         }
@@ -82,3 +84,41 @@ function nextMonth() {
 prev.addEventListener("click", prevMonth);
 next.addEventListener("click", nextMonth);
 
+
+todayBtn.addEventListener("click", () => {
+    today = new Date();
+    month = today.getMonth();
+    year = today.getFullYear();
+    initCalendar();
+});
+
+dateInput.addEventListener("keyup", (e) => {
+    dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+    if (dateInput.value.length === 2) {
+        dateInput.value += "/";
+    }
+    if (dateInput.value.length > 7) {
+        dateInput.value = dateInput.value.slice(0, 7);
+    }
+    if (e.inputType === "deleteContentBackward") {
+        if (dateInput.value.length === 3) {
+            dateInput.value = dateInput.value.slice(0, 2);
+        }
+    }
+});
+
+gotoBtn.addEventListener("click", gotoDate);
+
+function gotoDate() {
+    const dateArr = dateInput.value.split("/");
+
+    if (dateArr.length === 2) {
+        if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+            month = dateArr[0] - 1;
+            year = dateArr[1];
+            initCalendar();
+            return;
+        }
+    }
+    alert("invalid date");
+}
